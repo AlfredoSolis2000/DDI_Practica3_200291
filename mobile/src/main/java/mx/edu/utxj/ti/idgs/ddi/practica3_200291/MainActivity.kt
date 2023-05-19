ffd9aaada449cf1f578d665ebccf1d3dab74ee3d
AlfredoSolis2000 <84801802+AlfredoSolis2000@users.noreply.github.com>
2023-05-19 14:56:26 -0600

    first commit

---
+++ mobile/src/main/java/mx/edu/utxj/ti/idgs/ddi/practica3_200291/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.utxj.ti.idgs.ddi.practica3_200291
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
